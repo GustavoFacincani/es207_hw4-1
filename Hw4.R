52eadc0929e17de6c5d7d302e796d3495ee35979
Gustavo Facincani Dourado <57502337+GustavoFacincani@users.noreply.github.com>
2020-03-11 10:27:13 -0700

    Update HW4
    I got a little lost among the commands, I couldn't recognize everything you are doing here. For example, it took me a while to understand what is pargrai, pargrau, GM, ri, ru and what is happening with them, I wasn't sure about the results for the parametric intervals on 3.1 either. I think it would be helpful if you set the step-by-step process you are taking or making the functions more clear.
    Instead of plugging in numbers, your code could be more functional for other studies if you plugging variables. For example, instead of using "lowgr1<- ogr[5]+0.025*(ogr[6]-ogr[5])", you could use "lowgr1<- exp(mean.ln.grano-t.value*sqrt(var.grano/n))", in which you can plug in the numbers of the variables, for the lower or upper interval, or use them in other functions, without having to change them for each function all the time, so you can just change the variables and the rest is already done.

--- Hw4.R
+++ Hw4.R
@@ -3,69 +3,39 @@
 #the granodiorite data of exercise 2.3. Which is more appropriate for these data? Why?
 
 grandorCh<- c(6.0, 5.0, 0.5, 0.5, 0.6, 10, 0.4, 1.2, 0.2, 0.7, 0.3, 0.2, 0.8, 0.2, 1.7, 6.0, 0.5, 3.0)
-print(paste("Granodorite Chloride concentration in mg/L are:"))
-grandorCh
+x <- grandorCh[order(grandorCh)]
+#plot the data to visualize it
+plot(x)
+
+#Finding the non-parametric intervals
+
+#According to the table on page 367 of the book "Performance evaluation of computer and communication systems" (https://perfeval.epfl.ch/), the intervals for a dataset of 18 samples, the lower and upper bounds are the 5th and 14th numbers in the distribution (in ascending order). That gives us the Rl (Rl =  x' + 1) and Ru (Ru =  n ??? x' = x).
+
+lowerg <- x[5]
+upperg <- x[14]
+print(paste(x[5], "to", x[14], "mg/L"))
+
+#Finding the parametric intervals
+
+#Finding the parametric intervals
+n = 18
+alpha = 0.05
+t.value <- qt(1-alpha/2, n-1)
+t.value2 <- qt(1-alpha/2, n-1)
+ln.grano <- log(x)
+var.grano <- var(ln.grano)
+mean.ln.grano <- mean(ln.grano)
+
+lower <- exp(mean.ln.grano-t.value*sqrt(var.grano/n))
+lower
+
+upper <- exp(mean.ln.grano+t.value2*sqrt(var.grano/n))
+upper
+
+print(paste(lower, "to", upper, "mg/L"))
 
 
-#n=18 
-#alfa= 0.05 for 95% confidence
-#To ensure using the real median alfa/2=0.25
-#p=0.5 is the median
-
-#using the following link: https://www.youtube.com/watch?v=cxUWQCwxQgk and book https://perfeval.epfl.ch/ in page 347 
-
-#Then, x'=4, p=0.969 and:
-
-Ri<-5
-Ru<-14
-
-ogr<-sort(grandorCh)
-ogr
-median(ogr)
-mean(ogr)
-
-#using alfa/2=0.025
-
-lowgr1<- ogr[5]+0.025*(ogr[6]-ogr[5])
-uppgr1<- ogr[14]+ 0.025*(ogr[15]-ogr[14])
-print(paste(lowgr1, "to", uppgr1, "mg/L"))
-
-
-
-# I will make this a function
-n<-18
-y<-log(grandorCh)
-y1<- mean(y)
-y1
-GM<- exp(y1)
-GM
-a<-0.05/2
-#1-a=0.975
-t<- qt(0.975, df=18-1)
-t
-s<-var(y)
-s
-sqp<-sqrt(s/n)
-sqp
-
-
-
-pargrai<- exp(y1-(t*sqp))
-
-pargrau<- exp(y1+(t*sqp))
-
-print(paste(pargrai, "< GM<", pargrau))
-
-
-
-#cls in R 
-CI<-predict(lm(grandorCh~1), inetrval="confidence", level=0.95)
-CI[1]
-
-
-
-hist(grandorCh)
-
+#The non-parametric 95% interval estimates for the median is from 0.4 to 3 mg/L, while the parametric ranges from 0.5 to 1.8 mg/L. As the dataset is small and non-normal it is better to use non-parametric, because the non-parametric provides a wider range for the confidence interval than the parametric.
 
 
 #3.4 Construct the most appropriate 95 percent interval estimates for the mean and median
